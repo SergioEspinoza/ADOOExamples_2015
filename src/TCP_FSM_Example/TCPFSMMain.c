a2cfff65d08f5ab4da565ae04f23e0b6da398b75
Sergio Espinoza <sespinoza@localhost.localdomain>
2015-06-02 22:06:16 -0500

    integrating get char example

--- src/TCP_FSM_Example/TCPFSMMain.c
+++ src/TCP_FSM_Example/TCPFSMMain.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
 
 /** FW Profile function definitions */
 #include "FwProfile/FwSmConstants.h"
@@ -17,8 +18,53 @@
 /** TCPFSM function definitions */
 #include "TCPFSM.h"
 
+
+
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+
+
 int main(void)
 {
+    char inputChar;
+
 	/** Define the state machine descriptor (SMD) */
 	FwSmDesc_t smDesc = TCPFSMCreate(NULL);
 
@@ -33,14 +79,49 @@ int main(void)
 
 	/** Start the SM, send a few transition commands to it, and execute it */
 	FwSmStart(smDesc);
-	FwSmMakeTrans(smDesc, PassiveOpen);
-	FwSmMakeTrans(smDesc, ActiveOpen);
-	FwSmMakeTrans(smDesc, ReceiveSyn);
-	FwSmMakeTrans(smDesc, ReceiveAck);
-	FwSmMakeTrans(smDesc, Close);
-	FwSmMakeTrans(smDesc, ReceiveFIN);
-	FwSmMakeTrans(smDesc, CloseExt);
-	FwSmMakeTrans(smDesc, TimerExpire);
 
+
+    inputChar = getche( );
+
+    printf( "press e for exit\n" );
+
+
+    while( inputChar != 'e'  )
+    {
+
+        switch ( inputChar )
+        {
+
+            case  'p'    :
+                FwSmMakeTrans(smDesc, PassiveOpen);
+                break;
+            case 'a'    :
+                FwSmMakeTrans(smDesc, ActiveOpen);
+                break;
+            case 'r'    :
+                FwSmMakeTrans(smDesc, ReceiveSyn);
+                break;
+            case  's'   :
+                FwSmMakeTrans(smDesc, ReceiveAck);
+                break;
+            case  'c'   :
+                FwSmMakeTrans(smDesc, Close);
+                break;
+            case  'f'   :
+                FwSmMakeTrans(smDesc, ReceiveFIN);
+                break;
+            case  'e'   :
+                FwSmMakeTrans(smDesc, CloseExt);
+                break;
+            case  't'   :
+                FwSmMakeTrans(smDesc, TimerExpire);
+                break;
+
+            default:
+                printf( "unrecognized key" );
+                break;
+
+        }
+    }
 	return EXIT_SUCCESS;
 }
